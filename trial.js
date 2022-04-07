ad3f7b100fc201949f6240f396b73d04eaadb120
John Niyontwali <nijohn0006@gmail.com>
2022-04-07 02:30:20 +0200

    initial commit

---
+++ trial.js
@@ -0,0 +1,51 @@
+// let myFunction = () => {
+//     let dm = document.getElementById('demo');
+//     dm.innerHTML = "You Clicked!";
+//     dm.style.color ="red";
+//     document.getElementById('container').style.background = "gray";
+
+// }
+
+// let myFunction2 = () => {
+//     let dm = document.getElementById('demo');
+//     dm.innerHTML = "Back to Normal!";
+//     dm.style.color ="black";
+//     document.getElementById('container').style.background = "antiquewhite";
+
+// }
+
+// document.getElementById('container').addEventListener('click', myFunction)
+// document.getElementById('container').addEventListener('click', myFunction2)
+
+// let num1 = 0.1;
+// let num2 = 0.2;
+// let total = num1 + num2;
+// console.log(total);
+
+// let num = 'people';
+// console.log(isNaN(num));
+
+// console.log(typeof(NaN));
+// let str = 'John';
+// console.log(str.toUpperCase());
+
+// let myFunction = arr => { //You can drop off the paranthesis
+//     let result;
+//     for(let i = 0; i < arr.length; i++){
+//         result=arr[i];
+//         console.log(result);
+//     }
+// }
+
+// myFunction(['John','Sam','Niyontwali']);
+
+// Ternary Operator
+let myFunction = (num) => {
+    let compare = (num > 10) ? "The number is greater than 10" : "The number is less than 10";
+    console.log(compare)
+}
+
+myFunction(5);
+
+
+
